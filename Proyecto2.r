8b86343a742951a8f842a07d52e52ac68889f9bf
Laura Guacaneme <62676914+Guacaneme@users.noreply.github.com>
2022-06-25 03:43:46 -0500

    Entrega final proyecto

--- Proyecto2.r
+++ Proyecto2.r
@@ -25,6 +25,7 @@ for (fila in 1:10){
 
 n_aristas_aleatorio <- sample(1:294,1)   # 294 ya que para un grafo plano |E| ≤ 3|V|-6
 
+# Variables para las coordenadas de los vertices
 v_x <- vertices%%20+1
 v_y <- c()
 for (i in 1:10){
@@ -32,12 +33,13 @@ for (i in 1:10){
         v_y <- c(v_y,i)
     }
 }
-
+# Variables para las coordenadas de las aristas 
 ar_x0 <- c()
 ar_y0 <- c()
 ar_x1 <- c()
 ar_y1 <- c()
 
+# Función que escoge un vértice aleatorio cercano al vértice esogido al azar
 lado_aleatorio <- function(ver)(
     if (ver == 1 || ver == 20 || ver == 181 || ver == 200){
         sample(1:2,1)
@@ -53,14 +55,14 @@ lado_aleatorio <- function(ver)(
         }
     }
 )
-
+# Ciclo para escoger las n aristas aleatorias
 for (n in 1:n_aristas_aleatorio){
     while (TRUE){
         ver <- sample(1:200,1)  # Vértice aleatorio al cual se le va a poner una arista
         lado <- lado_aleatorio(ver)
         
         if (aristas[[ver]][lado] == 0){
-            ady <- c()
+            ady <- c()      # Variable que contiene los vértices adyacentes
             if (ver < 200){
                 ady <- c(ady,ver+1)
             }
@@ -76,18 +78,19 @@ for (n in 1:n_aristas_aleatorio){
             if (ver > 20){
                 ady <- c(ady,ver-20)
             }
-
+            # Se organizan los vértices adyacentes en orden ascendente
             ady_org <- c() 
             for (v in 1:length(ady)){
                 ady_org <- c(ady_org,min(ady))
                 ady <- ady[!(ady == min(ady))]
             }
-
+            # Se inserta la arista en la lista
             aristas[[ver]][lado] <- ady_org[lado]
-            
+            # Coordenadas de la arista
             ar_x0 <- c(ar_x0,v_x[ver])
             ar_y0 <- c(ar_y0,v_y[ver])
-
+            # Ya que es un grafo no dirigido, ambos vértices tienen la misma arista
+            # Se busca el vértice esgido al azar en los vértices adyacentes del otro vértice que compone la arista
             ady2 <- c()
             if (ady_org[lado] < 200){
                 ady2 <- c(ady2,ady_org[lado]+1)
@@ -114,6 +117,7 @@ for (n in 1:n_aristas_aleatorio){
             for (v in 1:length(ady_org2)){
                 if(ady_org2[v] == ver){
                     aristas[[ady_org[lado]]][v] <- ver
+                    # Coordenadas de la arista
                     ar_x1 <- c(ar_x1,v_x[ady_org[lado]])
                     ar_y1 <- c(ar_y1,v_y[ady_org[lado]])
                 }
@@ -122,6 +126,32 @@ for (n in 1:n_aristas_aleatorio){
         }
     }
 }
+
+
+print("En este grafo plano solo pueden haber subgrafos completos de hasta 3 vertices por la forma del grafo.")
+
+subgrafos <- list()
+
+for (s in 1:200){
+    subgrafos[[length(subgrafos)+1]] <- c(s)
+}
+
+copia_ar <- aristas
+for (v in 1:200){
+    for (t in 1:length(copia_ar[[v]])){
+        a <- copia_ar[[v]][t]
+        if (a != 0){
+            subgrafos[[length(subgrafos)+1]] <- c(v,a)
+            for (u in 1:1:length(copia_ar[[a]])){
+                if (copia_ar[[a]][u] == v){
+                    copia_ar[[a]][u] <- 0
+                }
+            }
+            copia_ar[[v]][t] <- 0
+        }
+    }
+}
+subgrafos
 
 plot(0,0, col="white", xlim = c(0,21), ylim = c(0,11), main = "Grafo Plano Aleatorio")
 points(v_x,v_y, col="red")
